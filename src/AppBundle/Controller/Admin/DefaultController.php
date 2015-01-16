1ce36a3e7d6f92cbb7080abddf605ed9cdb3d0ac
Jon Gotlin <jon@jon.se>
2015-01-16 15:03:40 +0100

    Simple admin

---
+++ src/AppBundle/Controller/Admin/DefaultController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use AppBundle\Entity\Connection;
+use AppBundle\Entity\ConnectionRequest;
+use AppBundle\Form\ConnectionRequestType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\VarDumper\VarDumper;
+
+/**
+ * @Route("admin")
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="admin_start")
+     */
+    public function indexAction(Request $request)
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        if ($request->isMethod('POST')) {
+            $learnerConnectionRequest = $this->getConnectionRequestRepository()->find($request->request->getInt('learner'));
+            $fluentSpeakerConnectionRequest = $this->getConnectionRequestRepository()->find($request->request->getInt('fluentSpeaker'));
+
+            $connection = new Connection();
+            $connection->setLearner($learnerConnectionRequest->getUser());
+            $connection->setFluentSpeaker($fluentSpeakerConnectionRequest->getUser());
+            $connection->setCity($learnerConnectionRequest->getCity());
+
+            $manager->persist($connection);
+            $manager->remove($learnerConnectionRequest);
+            $manager->remove($fluentSpeakerConnectionRequest);
+            $manager->flush();
+
+            return $this->redirect($this->generateUrl('admin_start'));
+        }
+
+        $learners = $this->getConnectionRequestRepository()->findBy(['wantToLearn' => true]);
+        $fluentSpeakers = $this->getConnectionRequestRepository()->findBy(['wantToLearn' => false]);
+
+        $parameters = [
+            'learners' => $learners,
+            'fluentSpeakers' => $fluentSpeakers,
+        ];
+
+        return $this->render('admin/default/index.html.twig', $parameters);
+    }
+
+    protected function getConnectionRequestRepository()
+    {
+        return $this->getDoctrine()->getManager()->getRepository('AppBundle:ConnectionRequest');
+    }
+
+}
