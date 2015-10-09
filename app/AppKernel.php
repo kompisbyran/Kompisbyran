40cf83353d9139d7e97f4ad35dfd825d6681ae7f
Jon Gotlin <jon@jon.se>
2015-10-09 21:44:45 +0200

    Removed oauth

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         );
 
