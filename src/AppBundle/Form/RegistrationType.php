0c9b3502d4f966a7208255d82deb0b336c031712
Jon Gotlin <jon@jon.se>
2015-10-25 08:42:57 +0100

    Added missing files

---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->remove('username');
+    }
+
+    /**
+     * @return string
+     */
+    public function getParent()
+    {
+        return 'fos_user_registration';
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'app_user_registration';
+    }
+}
