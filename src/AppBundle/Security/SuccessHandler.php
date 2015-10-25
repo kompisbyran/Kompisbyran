62148f74bf479a1710bc3e2feb2d97e66de6457a
Jon Gotlin <jon@jon.se>
2015-10-25 22:10:55 +0100

    Successhandler

---
+++ src/AppBundle/Security/SuccessHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class SuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    /**
+     * @var RouterInterface
+     */
+    protected $router;
+
+    /**
+     * @param RouterInterface $router
+     */
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * @param Request        $request
+     * @param TokenInterface $token
+     *
+     * @return RedirectResponse
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        foreach ($token->getRoles() as $role) {
+            if ('ROLE_ADMIN' == $role->getRole()) {
+                return new RedirectResponse($this->router->generate('admin_start'));
+            }
+        }
+
+        return new RedirectResponse($this->router->generate('homepage'));
+    }
+}
