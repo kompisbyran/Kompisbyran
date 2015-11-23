0a98acca7d4924c33485686b993b584b24981803
Jon Gotlin <jon@jon.se>
2015-11-23 22:04:26 +0100

    Local listener

---
+++ src/AppBundle/EventListener/LocaleListener.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class LocaleListener implements EventSubscriberInterface
+{
+    /**
+     * @var string
+     */
+    private $defaultLocale;
+
+    /**
+     * @param string $defaultLocale
+     */
+    public function __construct($defaultLocale = 'sv')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        if ($locale = $request->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+            $request->setLocale($locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            // must be registered before the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 17)),
+        ];
+    }
+}
