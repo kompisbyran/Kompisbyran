f1d34bc987ba899f6d0a1753e931d8de55d23035
Aldwin Sabornido <rifeman2007@gmail.com>
2016-03-27 17:44:15 +0800

    Moved the new_admin to admin2

--- src/AppBundle/Controller/Admin/DefaultController.php
+++ src/AppBundle/Controller/Admin/DefaultController.php
@@ -48,16 +48,15 @@ class DefaultController extends Controller
     /**
      * @Route("/{id}", name="admin_start", defaults={"id": null})
      * @Method("GET")
-     * @Template("admin/default/index.html.twig")
      */
     public function indexAction(Request $request, City $city = null)
     {
-        /*$manager = $this->getDoctrine()->getManager();
-
+        $manager = $this->getDoctrine()->getManager();
         if ($request->isMethod('POST')) {
+            /** @var ConnectionRequest $learnerConnectionRequest */
             $learnerConnectionRequest = $this->getConnectionRequestRepository()->find($request->request->getInt('learner'));
+            /** @var ConnectionRequest $fluentSpeakerConnectionRequest */
             $fluentSpeakerConnectionRequest = $this->getConnectionRequestRepository()->find($request->request->getInt('fluentSpeaker'));
-
             if ($this->getConnectionRepository()->findForUsers(
                 $learnerConnectionRequest->getUser(), $fluentSpeakerConnectionRequest->getUser()
             )) {
@@ -74,55 +73,75 @@ class DefaultController extends Controller
                 $connection->setFluentSpeakerComment($fluentSpeakerConnectionRequest->getComment());
                 $connection->setLearnerComment($learnerConnectionRequest->getComment());
                 $connection->setMusicFriend($learnerConnectionRequest->isMusicFriend());
-
                 $manager->persist($connection);
                 $manager->remove($learnerConnectionRequest);
                 $manager->remove($fluentSpeakerConnectionRequest);
                 $manager->flush();
-
                 $this->get('event_dispatcher')->dispatch(
                     DomainEvents::CONNECTION_CREATED,
                     new ConnectionCreatedEvent($connection)
                 );
-
                 $this->addFlash('info', sprintf(
                     'En koppling skapades melland %s och %s',
                     $learnerConnectionRequest->getUser()->getName(),
                     $fluentSpeakerConnectionRequest->getUser()->getName()
                 ));
             }
-
             return $this->redirect($this->generateUrl('admin_start'));
         }
-
         if (!$city) {
             $city = $this->getCityRepository()->findAll()[0];
         }
-
         $type = $request->query->get('type');
-
         $learners = $this->getConnectionRequestRepository()->findForCity($city, true, $type == 'musicfriend');
         $fluentSpeakers = $this->getConnectionRequestRepository()->findForCity($city, false, $type == 'musicfriend');
-
         $cities = $this->getCityRepository()->findAll();
-
         if ($type == 'musicfriend') {
             $categories = $this->getMusicCategoryRepository()->findAll();
         } else {
             $categories = $this->getGeneralCategoryRepository()->findAll();
-        }*/
-
-        $cities     = $this->cityManager->getFindAll();
-
-        if (!$city instanceof City) {
-            $city   = $cities[0];
         }
-
-        return [
-            'newUsers'          => $this->connectionRequestManager->getFindNewWithinCity($city),
-            'establishedUsers'  => $this->connectionRequestManager->getFindEstablishedWithinCity($city),
-            'cities'            => $cities,
-            'city'              => $city
+        $parameters = [
+            'categories' => $categories,
+            'learners' => $learners,
+            'fluentSpeakers' => $fluentSpeakers,
+            'cities' => $cities,
+            'city' => $city,
+            'type' => $type,
         ];
+        return $this->render('admin/default/index.html.twig', $parameters);
+    }
+
+    protected function getConnectionRequestRepository()
+    {
+        return $this->getDoctrine()->getManager()->getRepository('AppBundle:ConnectionRequest');
+    }
+
+    protected function getCityRepository()
+    {
+        return $this->getDoctrine()->getManager()->getRepository('AppBundle:City');
+    }
+
+    /**
+     * @return \AppBundle\Entity\ConnectionRepository
+     */
+    protected function getConnectionRepository()
+    {
+        return $this->getDoctrine()->getManager()->getRepository('AppBundle:Connection');
+    }
+
+    protected function getCategoryRepository()
+    {
+        return $this->getDoctrine()->getManager()->getRepository('AppBundle:Category');
+    }
+
+    protected function getMusicCategoryRepository()
+    {
+        return $this->getDoctrine()->getManager()->getRepository('AppBundle:MusicCategory');
+    }
+
+    protected function getGeneralCategoryRepository()
+    {
+        return $this->getDoctrine()->getManager()->getRepository('AppBundle:GeneralCategory');
     }
 }
