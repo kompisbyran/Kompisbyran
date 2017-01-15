f3c9a4efbcfcfc7ca6ad090967bbb3cf09ea9585
Jon Gotlin <jon@jon.se>
2017-01-15 11:43:54 +0100

    Meeting place in fixtures

--- src/AppBundle/DataFixtures/ORM/LoadData.php
+++ src/AppBundle/DataFixtures/ORM/LoadData.php
@@ -426,6 +426,7 @@ class LoadData extends AbstractFixture implements ContainerAwareInterface
             $municipality->setName($value);
             if ($i == 198) {
                 $municipality->setStartMunicipality(true);
+                $municipality->setMeetingPlace('på Slottet');
             }
             $manager->persist($municipality);
             $this->addReference('municipality-' . $i, $municipality);
